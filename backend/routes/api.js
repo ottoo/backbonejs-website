24bac87da597d1df7680bc354e9854a66ccf5640
Otto Kivikärki <otto-antti.kivikarki@cs.tamk.fi>
2014-07-25 21:19:01 +0300

    Login functionality is working

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,25 +4,55 @@ var User = require('../models/user');
 
 router.use(function(req, res, next) {
 	console.log('A request is happening' + ' ' + req.body.username + ' ' + req.body.password + ' ' + req.body.email);
+	console.log(req.url);
 	next();
 });
 
 router.get('/hello', function(req, res) {
-	res.json({ message: "Hei" });
+	res.json({
+		message: "Hei"
+	});
 });
 
+/*	Insert new user to the database 	*/
 router.post('/users', function(req, res) {
 	var user = new User();
 	user.username = req.body.username;
 	user.password = req.body.password;
 	user.email = req.body.email;
-	
+
 	user.save(function(err) {
-		if (err) { 
-			res.json({ message: err.code });
+		if (err) {
+			res.json({
+				message: err.code
+			});
 		}
-		res.json({ message: 'New user created!' });
+		res.json({
+			message: 'New user created!'
+		});
 	});
+});
+
+/*	Authenticate user 	*/
+router.post('/login', function(req, res) {
+	User.findOne({
+		username: req.body.username
+	}, function(err, user) {
+		if (err) throw err;
+
+		if (user != null) {
+			user.comparePasswords(req.body.password, function(err, result) {
+				if (err) throw err;
+				res.json({
+					isValidUser: result
+				});
+			});
+		} else {
+			res.json({
+				message: 'User not found'
+			});
+		}
+	});	
 });
 
 module.exports = router;
